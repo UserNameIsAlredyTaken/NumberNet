bc735fc4303fa40fb73d95bbb45b3dba3c9a0c48
UserNameIsAlredyTaken <gdaniltatarinof@gmail.com>
2017-11-16 00:16:28 +0300

    Часть инициации весов и биасов

--- Net/Net/net.c
+++ Net/Net/net.c
@@ -4,13 +4,24 @@ typedef struct net {
 	int n_layers;
 	int* sizes;
 	int** biases;
-	int** weights;
+	int*** weights;
 }net;
 
 net* init_net(int* sizes,int n_layers){
 	net* the_net = malloc(sizeof(net));
 	the_net->sizes = sizes;
 	the_net->n_layers = n_layers;
-	th
+	for (int i = 1; i < n_layers;i++){//заполняем биасы для каждого нейронного слоя кроме первого
+		for (int j = 0; j < sizes[i];j++){//заполняем биасы для каждого нейрона
+			//здесь могла бы быть ваша рандомящая функция с диапозоном от -1 до 1
+		}
+	}
+	for (int i = 1; i < n_layers; i++) {//заполняем веса для каждого нейронного слоя кроме первого
+		for (int j = 0; j < sizes[i]; j++){//заполняем веса для каждого нейрона
+			for(int k = 0; k<sizes[i-1]; k++){//для каждого нейрона соответствующий вес с нейроном из предыдущего слоя
+				//здесь могла бы быть ваша рандомящая функция с диапозоном от -1 до 1
+			}
+		}
+	}
 	return the_net;
 }
